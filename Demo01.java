f15f04a8b8d9e9e806e844323a9c665be77639dc
Beidou&Kazuha <55118194+yezitai919@users.noreply.github.com>
2021-12-09 14:37:27 +0800

    Update Demo01.java

--- Demo01.java
+++ Demo01.java
@@ -31,7 +31,6 @@ public class Demo01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int cases = sc.nextInt();
-        System.out.println();
         for (int i = 0; i < cases; i++) {
             n = sc.nextInt();
             m = sc.nextInt();
